93650472197b3940242a52e892b01c4b3668a71a
Lewis Muthaura <lewislemmu10@gmail.com>
2024-04-29 15:17:36 -1200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home () {
+  return (
+<section className="max-w-lg mx-auto text-center">
+  <h1 className="text-6xl font-bold">
+    Boost your <br />
+    creative   work
+  </h1>
+  <h2 className="mt-4 mb-8">
+    Accept support for your work.<br />
+     It's easier than you think.
+  </h2>
+  <button className="bg-purple-300 px-8 py-4 font-bold rounded-full">
+    Launch my page
+  </button>
+</section>  
+  );
+}
